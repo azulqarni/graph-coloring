1fc4e987be4779a72a0a0f8949b322f974689428
azulqarni <44493985+azulqarni@users.noreply.github.com>
2022-03-20 01:16:39 -0400

    Add files via upload

--- graphgen.c
+++ graphgen.c
@@ -28,7 +28,7 @@ int main (int argc, char *argv[]) {
     srand(time(NULL));
     for (i = 0; i < N; i++)
         for (j = i; j < N; j++)
-			//arr[j][i] = arr[i][j] = j == i ? 0 : rand() % 10 < 3;
+            //arr[j][i] = arr[i][j] = j == i ? 0 : rand() % 10 < 3;
             arr[j][i] = arr[i][j] = j == i ? 0 : rand() % 2;
 
     FILE *fp = TXT ? fopen ("graph.txt", "w") : stdout;
